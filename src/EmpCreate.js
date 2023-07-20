be2cbe8c59b1028f3f34a8abe3b2e83a9aa86cd9
abirbaccara <abirbaccara96@gmail.com>
2023-07-20 13:39:33 +0100

    cruds-project

---
+++ src/EmpCreate.js
@@ -0,0 +1,93 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+const EmpCreate = () => {
+
+    const[id, idchange]=useState("");
+    const[name, namechange]=useState("");
+    const[email, emailchange]=useState("");
+    const[phone, phonechange]=useState("");
+    const[active, activechange]=useState(true);
+    const[validation, valchange]=useState(false);
+
+    const navigate=useNavigate();
+
+
+    const handlesubmit = (e) => {
+        e.preventDefault();
+
+        const empdata={name, email, phone, active};
+
+        fetch("http://localhost:8000/employee", {
+
+            method: "POST",
+            headers: {"content-type" : "application/json"},
+            body: JSON.stringify(empdata),
+        }).then((res) => {
+            alert("Saved Successfully.")
+            navigate("/");
+        }).catch((err) => {
+            console.log(err.message)
+        })
+    }
+
+    return (
+        <div>
+            <div className="row">
+                <div className="offset-lg-3 col-lg-6">
+                    <form className="container" onSubmit={handlesubmit}>
+                        <div className="card" style={{"textAlign" : "left"}}>
+                            <div className="card-title">
+                                <h2>Employee Create</h2>
+                            </div>
+                            <div className="card-body">
+                                <div className="row">
+                                    <div className="col-lg-12">
+                                        <div className="from-group">
+                                            <label>ID</label>
+                                            <input value={id} disabled="disabled" className="form-control"></input>
+                                        </div>
+                                        <div className="col-lg-12">
+                                            <div className="from-group">
+                                                <label>Name</label>
+                                                <input required value={name} onMouseDown={e=>valchange(true)} onChange={e=>namechange(e.target.value)} className="form-control"></input>
+                                            {name.length==0 && validation && <span className="text-danger">Enter the Name</span>}
+                                            </div>
+                                        </div>
+                                        <div className="col-lg-12">
+                                            <div className="from-group">
+                                                <label>Email</label>
+                                                <input value={email} onChange={e=>emailchange(e.target.value)} className="form-control"></input>
+                                            </div>
+                                        </div>
+                                        <div className="col-lg-12">
+                                            <div className="from-group">
+                                                <label>Phone</label>
+                                                <input value={phone} onChange={e=>phonechange(e.target.value)} className="form-control"></input>
+                                            </div>
+                                        </div>
+                                        <div className="col-lg-12">                                    <div className="col-lg-12">
+                                            <div className="form-check">
+                                            <input type="checkbox" className="form-check-input"></input>
+                                                <label checked={active} onChange={e=>activechange(e.target.value)} className="form-check-label">Is Active</label>
+                                            </div>
+                                        </div>
+                                        <div className="col-lg-12">
+                                            <div className="form-group">
+                                                <button type="submit" className="btn btn-success">Save</button>
+                                                <Link to="/" className="btn btn-danger">Back</Link>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default EmpCreate;
